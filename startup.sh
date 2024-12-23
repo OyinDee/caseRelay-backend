c0b50c0d34a97c460246eb85043c9ae84ec9b6c6
OyinDee <ajalaoyinda@gmail.com>
2024-12-23 23:49:46 +0100

    last commit

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd "${BASH_SOURCE%/*}" || exit
-export ASPNETCORE_URLS="http://+:80" 
+export ASPNETCORE_URLS="http://+:80"  # Standard HTTP port
 export ASPNETCORE_ENVIRONMENT="Production"
 dotnet CaseRelayAPI.dll
