18558ccb9e7c7b79fbf8c928b19e90c753fc7551
OyinDee <99656110+OyinDee@users.noreply.github.com>
2024-12-28 18:19:04 +0100

    Update IUserService.cs

--- services/IUserService.cs
+++ services/IUserService.cs
@@ -8,7 +8,9 @@ namespace CaseRelayAPI.Services
         Task<User?> GetUserByPoliceIdAsync(string policeId);
         Task<User?> GetUserByIdAsync(int userId);
         Task<List<User>> GetAllUsersAsync();
-        Task<User?> UpdateUserAsync(User user);
+    
+    Task<User?> UpdateUserAsync(User user, bool isAdminOperation = false); 
+        
         Task<bool> DeleteUserAsync(int userId);
         Task<User?> CreateUserAsync(User newUser);
     }
