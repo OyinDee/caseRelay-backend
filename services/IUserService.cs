0df17d04cf3653ff5fb834816328a825853d9b3e
OyinDee <99656110+OyinDee@users.noreply.github.com>
2024-12-28 22:53:48 +0100

    Update IUserService.cs

--- services/IUserService.cs
+++ services/IUserService.cs
@@ -1,17 +1,10 @@
-using CaseRelayAPI.Models;
-using System.Threading.Tasks;
-
-namespace CaseRelayAPI.Services
+public interface IUserService
 {
-    public interface IUserService
-    {
-        Task<User?> GetUserByPoliceIdAsync(string policeId);
-        Task<User?> GetUserByIdAsync(int userId);
-        Task<List<User>> GetAllUsersAsync();
-    
-    Task<User?> UpdateUserAsync(User user, bool isAdminOperation = false); 
-        
-        Task<bool> DeleteUserAsync(int userId);
-        Task<User?> CreateUserAsync(User newUser);
-    }
+    Task<User?> GetUserByPoliceIdAsync(string policeId);
+    Task<User?> GetUserByIdAsync(int userId);
+    Task<List<User>> GetAllUsersAsync();
+    Task<User?> UpdateUserAsync(User user, bool isAdminOperation = false);
+    Task<bool> DeleteUserAsync(int userId);
+    Task<User?> CreateUserAsync(User newUser);
+    Task<User?> CreateUserWithPasswordAsync(User newUser, string temporaryPassword); 
 }
